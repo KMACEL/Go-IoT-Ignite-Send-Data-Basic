ef23965986399f30d0402b1a22a01a60691ee031
Mert Acel <mert.acel@ardictech.com>
2018-01-25 09:24:29 +0300

    Paramater Change

--- GO_IoT_Ignite_Send_Message.go
+++ GO_IoT_Ignite_Send_Message.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"strconv"
+	"time"
+
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
-
 func main() {
-	topic := "golang@gopher@mqtt/publish/DeviceProfile/goNode/goSensor"
+	topic := "gopher@go/publish/DeviceProfile/ArdicOffice/HumidityGo"
 	broker := "ssl://mqtt.ardich.com:8883"
 
-	clientId := "golang@gopher@mqtt"
+	clientId := "gopher@go"
 	user := "gophergo"
 	password := "12345678"
-	
+
 	qos := 1
 
-	payload:= "{data:{sensorData:[{date:"+strconv.FormatInt(time.Now().Unix(), 10)+"000,values:[555]}],formatVersion:2}}"
+	payload := "{data:{sensorData:[{date:" + strconv.FormatInt(time.Now().Unix(), 10) + "000,values:[555]}],formatVersion:2}}"
 
 	opts := MQTT.NewClientOptions()
 	opts.AddBroker(broker)
@@ -26,18 +26,18 @@ func main() {
 	opts.SetUsername(user)
 	opts.SetPassword(password)
 
-
 	client := MQTT.NewClient(opts)
 
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
-	
+
 	fmt.Println("Sending Data...")
 	token := client.Publish(topic, byte(qos), false, payload)
 	token.Wait()
-		
+
 	client.Disconnect(250)
+	client.Connect()
 	fmt.Println("Data Sending Completed...")
-	
+
 }
